1ec3d366e32e213f8c83645587e6f415c86cbf41
Daisy Pennartz <daisypenn99@gmail.com>
2024-01-02 23:10:28 -0600

    Create an About page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,13 @@
 import "./App.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Import the components
 import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
-const router = createBrowserRouter([{ path: "/", element: <Home /> }]);
-
+const router = createBrowserRouter([
+    { path: "/", element: <Home /> },
+    { path: "/about", element: <About /> }, // Add a route for the About component
+]);
 function App() {
   return <RouterProvider router={router} />;
 }
--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,6 +1,5 @@
 import {type FC} from "react";
 import Header from "../UI/Header";
-import Button from "../UI/Button";
 
 const Home: FC = () => {
   return (
