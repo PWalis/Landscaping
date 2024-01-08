8bbec69bc15be540e691249550b9945ac31de81b
Daisy Pennartz <daisypenn99@gmail.com>
2024-01-07 23:05:02 -0600

    Footer added + UI changes for header and main section

---
+++ frontend/src/components/UI/TestimonialCard.tsx
@@ -0,0 +1,25 @@
+import {type FC} from "react";
+
+interface TestimonialProps {
+    img: string;
+    text: string;
+    author: string;
+    area: string;
+}
+const TestimonialCard: FC<TestimonialProps> = ({img , text, author, area}) => {
+    return (
+        <div className="flex flex-col bg-stone-100/80 rounded-lg shadow-lg h-[600px] w-[400px]">
+            <img className="object-cover h-1/2 shadow-lg" src={img}
+                 alt="Testimonial Image"/>
+            <div className="m-4 text-green-900/50">
+                <p className="text-xl"> {text} </p>
+                <br/>
+                <div className="mt-auto">
+                    <p> - {author} </p>
+                    <p> located in {area} </p>
+                </div>
+            </div>
+        </div>
+    );
+};
+export default TestimonialCard;
