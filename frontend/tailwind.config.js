7c9cbc0504ad3846d6c2fbbe28e2ba58a1fb110f
Daisy Pennartz <daisypenn99@gmail.com>
2024-02-21 17:56:26 -0700

    Update GalleryContent and GalleryPictures Sections for the Gallery Page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  content: [
+      "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/flowbite-react/lib/esm/**/*.js',
+  ],
   theme: {
     fontFamily: {
       sans: ['Playfair Display', 'sans-serif'],
@@ -8,5 +12,7 @@ export default {
     },
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 };
