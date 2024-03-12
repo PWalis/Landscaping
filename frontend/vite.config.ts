0bd18cce4726bb3a6a59ff0f1143faab70033f04
PWalis <pwilky1016@gmail.com>
2024-03-12 16:24:42 -0700

    added env variable for front end request proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3307",
+        target: BASE_URL,
         rewrite: (path) => path.replace(/^\/api/, ""),
       }
     }
