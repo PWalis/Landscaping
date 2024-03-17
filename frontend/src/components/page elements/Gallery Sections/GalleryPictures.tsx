430c7c8451d153f276aa2ae9ebc0bf146000e8d7
PWalis <pwilky1016@gmail.com>
2024-03-17 13:23:18 -0700

    Frontend: Dynamic image list depending on length of galleryitems implemented

--- frontend/src/components/page elements/Gallery Sections/GalleryPictures.tsx
+++ frontend/src/components/page elements/Gallery Sections/GalleryPictures.tsx
@@ -2,15 +2,20 @@ import "../../../index.css";
 import galleryImage from "../../../assets/sedona_1b.jpg"
 import galleryImage2 from "../../../assets/testimonials-image-2.jpg"
 import galleryImage3 from "../../../assets/after-pic.jpg"
+import { type FC, PropsWithChildren } from "react";
 
+interface GalleryPicturesProps extends PropsWithChildren<{}> {
+    showLastImage: boolean;
+}
 
-const GalleryPictures: React.FC = () => {
+const GalleryPictures: React.FC<GalleryPicturesProps> = ( props ) => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2">
             <div className="overflow-hidden"><img className="w-full h-full object-cover" src={galleryImage} alt="" /></div>
             <div className="overflow-hidden"><img className="w-full h-full object-cover" src={galleryImage2} alt="" /></div>
             <div className="overflow-hidden"><img className="w-full h-full object-cover" src={galleryImage3} alt="" /></div>
-            <div className="overflow-hidden"><img className="w-full h-full object-cover" src={galleryImage} alt="" /></div>
+            {props.showLastImage && <div className="overflow-hidden"><img className="w-full h-full object-cover" src={galleryImage} alt="" /></div>}
+            {props.children}
         </div>
     );
 };
